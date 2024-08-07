4748839850656bf897d227d5584b00df4789d026
Ian Andrew Macalisang <andyware@live.com>
2024-08-07 09:12:06 +0800

    updated with key

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { useChat } from "ai/react";
-import { Bot, Loader, Loader2, Send, User2 } from "lucide-react";
-import Image from "next/image";
+import { Bot, Loader2, Send, User2 } from "lucide-react";
 import Markdown from "./component/markdown";
+
+import logo from "../public/meta-logo-facebook.svg";
+
+import Image from "next/image";
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
@@ -10,11 +13,33 @@ export default function Home() {
       api: "api/llm-response",
     });
   return (
-    <main className="flex min-h-screen flex-col items-center p-12 text-black">
-      {/* form element */}
-      {RenderForm()}
-      {RenderMessages()}
-      {/* rendering messages */}
+    <main className="flex min-h-screen flex-col items-center p-12 text-black bg-slate-100">
+      <div className="mb-4 flex flex-row gap-4">
+        <div>
+          <Image
+            src={logo}
+            alt="logo"
+            width={63}
+            height={63}
+            className="-mt-1"
+          />
+        </div>
+        <div>
+          <h1 className="text-2xl font-black">Ian Macalisang (Experimental)</h1>
+          <p>Model: Llama 3.1 8B</p>
+        </div>
+      </div>
+      <div className="w-full max-w-3xl border-gray-200 border-2 rounded-xl bg-white p-5">
+        {RenderForm()}
+        <br />
+        {RenderMessages()}
+      </div>
+      <div className="text-sm text-gray-500 font-semibold mt-3">
+        An Ian Macalisang Production
+      </div>
+      <p className="text-sm text-gray-500">
+        Experimental projects with open-source AI.
+      </p>
     </main>
   );
 
@@ -34,11 +59,13 @@ export default function Home() {
       >
         <input
           type="text"
-          placeholder={isLoading ? "Generating . . ." : "ask something . . . "}
+          placeholder={
+            isLoading ? "Nag-iisip ng isasagot sayo..." : "Ano kailangan mo?"
+          }
           value={input}
           disabled={isLoading}
           onChange={handleInputChange}
-          className="border-b border-dashed outline-none w-full px-4 py-2 text-[#0842A0] placeholder:text-[#0842A099] text-right focus:placeholder-transparent disabled:bg-transparent"
+          className="border-b outline-none w-full px-4 py-2 text-[#0842A0] placeholder:text-[#0842A099] focus:placeholder-transparent disabled:bg-transparent"
         />
         <button
           type="submit"
@@ -59,20 +86,27 @@ export default function Home() {
 
   function RenderMessages() {
     return (
-      <div id="chatbox" className="flex flex-col-reverse w-full text-left mt-4 gap-4 whitespace-pre-wrap">
+      <div
+        id="chatbox"
+        className="flex flex-col-reverse w-full text-left mt-4 gap-4 whitespace-pre-wrap"
+      >
         {messages.map((m, index) => {
           return (
             <div
-              className={`p-4 shadow-md rounded-md ml-10 relative ${
-                m.role === "user" ? "bg-stone-300" : ""
+              className={`p-4 shadow-lg rounded-md ml-10 relative ${
+                m.role === "user" ? "bg-slate-100" : ""
               }`}
             >
               <Markdown text={m.content} />
               {m.role === "user" ? (
-                <User2 className="absolute top-2 -left-10 border rounded-full p-1 shadow-lg" />
+                <User2
+                  size={32}
+                  className="absolute top-1 -left-10 border rounded-full p-2 shadow-lg"
+                />
               ) : (
                 <Bot
-                  className={`absolute top-2 -left-10 border rounded-full p-1 shadow-lg stroke-[#0842A0] ${
+                  size={32}
+                  className={`absolute top-1 -left-10 border rounded-full p-2 shadow-lg stroke-[#0842A0] ${
                     isLoading && index === messages.length - 1
                       ? "animate-bounce"
                       : ""
