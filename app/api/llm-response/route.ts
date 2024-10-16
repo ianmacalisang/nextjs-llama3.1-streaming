55e2b1be3b9b3fb64b85fb063265c34bd33cf512
Ian Andrew Macalisang <dev@ianmacalisang.com>
2024-10-16 20:36:54 +0800

    ai server changed

--- app/api/llm-response/route.ts
+++ app/api/llm-response/route.ts
@@ -5,11 +5,11 @@ export async function POST(req: Request, res: Response) {
   const { messages } = await req.json();
 
   const openai = createOpenAI({
-    baseURL: "https://openai.ianmacalisang.com/v1/",
-    apiKey: "ollama",
+    baseURL: "https://openai.hub.ianmacalisang.com/api",
+    apiKey: "sk-bcf9f383735447b197be3eeb46702649",
   });
   const result = await streamText({
-    model: openai("qwen2.5:1.5b"),
+    model: openai("llama3.2:latest"),
     messages,
     system: `You are a web developer that specialize in nextjs, tailwindcss and shadcn ui. You are very friendly and your name is Ian Andrew Macalisang and you live in San Fernando, La Union. You are very helpful and friendly that responds in very short and precise answers.`,
   });
