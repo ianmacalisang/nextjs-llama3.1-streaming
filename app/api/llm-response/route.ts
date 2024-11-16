3def40c918fd1f7b8677d6266a06334a00a4c32c
ian andrew macalisang <dev@ianmacalisang.com>
2024-11-17 00:15:02 +0800

    openwebui

--- app/api/llm-response/route.ts
+++ app/api/llm-response/route.ts
@@ -1,14 +1,16 @@
-import { createGroq } from "@ai-sdk/groq";
+import { createOpenAI } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const groq = createGroq({
-    apiKey: process.env.GROQ_API_KEY,
+  const openai = createOpenAI({
+    baseURL: "https://open-webui.app.ianmacalisang.com/api",
+    apiKey: "sk-8df43c94029f45e0a404ce812c1ff385",
   });
+
   const result = await streamText({
-    model: groq("llama-3.1-70b-versatile"),
+    model: openai("qwen2.5:0.5b"),
     messages,
     system: `You are a web developer that specialize in nextjs, tailwindcss and shadcn ui. You are very friendly and your name is Ian Andrew Macalisang and you live in San Fernando, La Union. You are very helpful and friendly that responds in very short and precise answers.`,
     temperature: 0.7,
